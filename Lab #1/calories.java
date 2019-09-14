e4c490f3edd10edf68571ed9dc1f8ef4d48829ac
byCM <43051181+byCM@users.noreply.github.com>
2019-09-14 12:39:06 -0800

    Update calories.java

--- Lab #1/calories.java
+++ Lab #1/calories.java
@@ -1,79 +1,32 @@
-// Lab #2
-
 import java.util.Scanner;
 
-class grades {
-	public static void main(String args []) {
-		Scanner keyboard = new Scanner(System.in);
-		
-		double finalGrade;
-		char grade;		
-		int g1, g2, g3, g4, g5, g6, g7, g8;
-		
-		//Weights your scores and adds them together to give you your final percent/grade of the class.
-		
-		System.out.println("zyBooks Participation Activities: ");
-		g1 = keyboard.nextInt();
-		double finalPart = (double) g1 *.05;
-		System.out.printf("%.2f points earned for zyBooks Participation Activities Grade\n", finalPart);
-		
-		System.out.println("zyBooks Challenge Activities:");
-		g2 = keyboard.nextInt();
-		double finalChallenge = (double) g2 * .05;
-		System.out.printf("%.2f points earned for zyBooks Challenge Activities\n", finalChallenge);
-		
-		System.out.println("Lecture Attendance:");
-		g3 = keyboard.nextInt();
-		double finalAttendance = (double) g3 * .05;
-		System.out.printf("%.2f points earned for Lecture attendace\n", finalAttendance);
-		
-		System.out.println("Laboratory attendance and exercises:");
-		g4 = keyboard.nextInt();
-		double finalLab = (double) g4 * .15;
-		System.out.printf("%.2f points earned for Laboratory attendance and exercises\n", finalLab);
-		
-		System.out.println("Homework:");
-		g5 = keyboard.nextInt();
-		double finalHomework = (double) g5 * .20;
-		System.out.printf("%.2f points earned for Homework\n", finalHomework);
-		
-		System.out.println("Mid‐term exam I:");
-		g6 = keyboard.nextInt();
-		double finalMid1 = (double) g6 * .15;
-		System.out.printf("%.2f points earned for Mid‐term exam I\n", finalMid1);
-		
-		System.out.println("Mid‐term exam II:");
-		g7 = keyboard.nextInt();
-		double finalMid2 = (double) g7 * .15;
-		System.out.printf("%.2f points earned for Mid‐term exam II\n", finalMid2);
-
-		System.out.println("Final exam: ");
-		g8 = keyboard.nextInt();
-		double finalExam = (double) g8 *.20;
-		System.out.printf("%.2f points earned for Final Exam\n", finalExam);
-		
-		finalGrade = finalPart + finalChallenge + finalAttendance + finalLab + finalHomework + finalMid1 + finalMid2 + finalExam;
-		keyboard.close();
-		
-		if (finalGrade >= 90) {
-			grade = 'A';
-		} 
-		else if (finalGrade >= 80 ){
-			grade = 'B';	
-		} 
-		else if (finalGrade >= 70) {
-			grade = 'C';
-		} 
-		else if (finalGrade >= 60) {
-			grade = 'D';
-		} 
-		else { 
-			grade = 'F';
-		}
-		
-		System.out.printf("You got a %C!, Your Final Grade is: %.2f/100.00\n", grade, finalGrade);
-		
-		
-		}
-	}
-  
+public class LabProgram {
+   public static void main(String[] args) {
+      
+      double userAge, userWeight, userHeart, userTime, userAgeTimes,
+      menAge, menWeight, menHeart, menTime, finalMenCalories, finalWomenCalories,
+      womenAge, womenHeart, womenTime;
+      
+      
+      Scanner scan = new Scanner(System.in);
+      userAge = scan.nextDouble();
+      userWeight = scan.nextDouble();
+      userHeart = scan.nextDouble();
+      userTime = scan.nextDouble();
+      
+      menAge = (userAge * 0.2017) - (userWeight * 0.09036);
+      menHeart = (userHeart * 0.6309) - 55.0969;
+      menTime = userTime / 4.184;
+      
+      womenAge = (userAge * 0.074) - (userWeight *  0.05741);
+      womenHeart = (userHeart * 0.4472) - 20.4022;
+      womenTime = userTime / 4.184;
+      
+      finalMenCalories = (menAge + menHeart) * userTime / 4.184;
+      finalWomenCalories = (womenAge + womenHeart) * userTime / 4.184;   
+      
+      System.out.printf("Men: %.2f calories\n", finalMenCalories);
+      System.out.printf("Women: %.2f calories\n", finalWomenCalories);
+      
+   }
+} 
