626209c3acccd46bf5cc0db575c2a340d7804249
byCM <43051181+byCM@users.noreply.github.com>
2019-09-18 19:26:11 -0800

    Update highways.java

--- Assignments/A2/highways.java
+++ Assignments/A2/highways.java
@@ -9,23 +9,28 @@ public class LabProgram {
 
       highwayNumber = scnr.nextInt();
       
-      
       do {
          if (highwayNumber % 2 == 0) {
-            System.out.println("The " + highwayNumber + " primary, going east/west.");
+            System.out.println("The " + highwayNumber + " is primary, going east/west.");
             break;
          } else if (highwayNumber % 2 == 1) {
-            System.out.println("The " + highwayNumber + " primary, going north/south.");
+            System.out.println("The " + highwayNumber + " is primary, going north/south.");
             break;
          }
       } while ((highwayNumber <= 99) && (highwayNumber >= 1));
-
       
+   
       do {
          digit = highwayNumber % 100;
          System.out.println("The " + highwayNumber + " is auxiliary, serving the " + digit + ", going east/west.");
          break;
       } while ((highwayNumber <= 999) && (highwayNumber >= 100));
       
+         
+      do {
+         System.out.println("0 is not a valid interstate highway number.");
+         break;
+     } while ((highwayNumber < 1) || (highwayNumber > 999));
+   
    }
 }
