b67c9effca2405d0b3a09efd8fa3c4b2a771ed37
byCM <43051181+byCM@users.noreply.github.com>
2019-10-02 13:21:45 -0800

    Update batman.java

--- Assignments/A3/batman.java
+++ Assignments/A3/batman.java
@@ -41,3 +41,26 @@ else if (firstDigit + secondDigit + thirdDigit + fourthDigit == 27) {
 	System.out.println(firstDigit + "" + secondDigit + "" + thirdDigit + "" + fourthDigit);
 } 
 */
+
+
+public class batman {
+
+	public static void main(String[] args) {	
+		
+		for (int i = 1000; i < 9999; i++) {
+			int f = Integer.parseInt(Integer.toString(i).substring(0, 1));
+			int s = Integer.parseInt(Integer.toString(i).substring(1, 2));
+			int t = Integer.parseInt(Integer.toString(i).substring(2, 3));
+			int fourth = Integer.parseInt(Integer.toString(i).substring(3, 4));
+
+			
+			if ((f != s && f != t && f != fourth && s != f && s != t && s != fourth && t != f && t != s && t != fourth
+					&& fourth != f && fourth != s && fourth != t) && (f == t * 3) && (f + s + t + fourth == 27) && (fourth % 2 == 1)) {
+				System.out.println(f + "" + s + "" + t + "" + fourth);
+			} 
+
+			
+		}
+	}
+
+}
