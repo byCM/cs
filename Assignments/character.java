7b4f08461a66cf9b23df31aac76363e01b7c96c7
byCM <43051181+byCM@users.noreply.github.com>
2019-10-04 22:40:31 -0800

    Delete character.java

--- Assignments/character.java
+++
@@ -1,9 +0,0 @@
-
-      if ((Character.isLetter(passCode.charAt(0))) && (Character.isLetter(passCode.charAt(1)))) {
-         System.out.println("Alphabetic at 0");
-         System.out.println("Alphabetic at 1");
-      } else if (Character.isLetter(passCode.charAt(0))) {
-         System.out.println("Alphabetic at 0");
-      } else if (Character.isLetter(passCode.charAt(1))) {
-         System.out.println("Alphabetic at 1");
-      }
