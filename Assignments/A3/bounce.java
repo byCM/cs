c344e005a63a84ad917a17449df4db92f2782fdf
byCM <43051181+byCM@users.noreply.github.com>
2019-10-04 18:50:11 -0800

    Update bounce.java

--- Assignments/A3/bounce.java
+++ Assignments/A3/bounce.java
@@ -1,53 +1,5 @@
 import java.util.Scanner;
 
-public class bounce {
-
-	public static void main(String[] args) {
-		int time = 0;
-		double height = 0;
-		double velocity = 0;
-		int i;
-		
-		Scanner keyboard = new Scanner(System.in);
-		
-		
-		for (i = 0; i >= velocity; i++) {
-			do {
-				System.out.print("Please enter the initial velocity: ");
-				if (keyboard.hasNextInt()) {
-				    velocity = keyboard.nextDouble();
-				    System.out.println("Initial velocity: " + velocity);
-				    
-				    height = 0;
-				    time = 0;
-				    System.out.println("Time: " + time + "       Height: " + height + "        Velocity: " + velocity);
-				    for (i = 0; height >= 0; i ++) {
-				    	height += velocity;
-				    	velocity -= 32;
-				    	time++;
-				    	System.out.println("Time: " + time + "       Height: " + height + "        Velocity: " + velocity);
-				    	if(height < 0) {
-				    		System.out.println("Bounce 1");
-				    		for (i = 0; i >= 0; i++) {
-				    		}
-				    	}
-				    }
-				    
-				} else {
-				    System.out.println("I need an int, please try again.");
-				    keyboard.nextDouble();
-				}
-			} while (velocity <= 0);
-			
-		}
-
-			
-	}
-}
-
-
-import java.util.Scanner;
-
 
 public class bounce {
 
