f37786c51e7bfd6b5d36ed6628f374e6768ea4a2
byCM <43051181+byCM@users.noreply.github.com>
2019-10-15 15:17:23 -0800

    Create weekday.java

---
+++ Assignments/A4/weekday.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class week {
+
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		String day, month, year;
+		
+		
+		day = keyboard.next();
+		month = keyboard.next();
+		year = keyboard.next();
+		
+		System.out.println(day + " " + month + " " + year);
+		
+		
+		
+
+	}
+
+}
