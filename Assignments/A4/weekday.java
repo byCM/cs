b57bc24aac48f4ba6a8c40d661e445063802bc52
byCM <43051181+byCM@users.noreply.github.com>
2019-10-15 15:31:06 -0800

    Update weekday.java

--- Assignments/A4/weekday.java
+++ Assignments/A4/weekday.java
@@ -3,19 +3,21 @@ import java.util.Scanner;
 public class week {
 
 	public static void main(String[] args) {
+		getMonth();
+
+	}
+	
+	static void getMonth() {
 		Scanner keyboard = new Scanner(System.in);
 		String day, month, year;
 		
-		
+		System.out.println("Please enter a date in numbers only (EX: 12 3 2020)");
 		day = keyboard.next();
 		month = keyboard.next();
 		year = keyboard.next();
 		
-		System.out.println(day + " " + month + " " + year);
+		System.out.println(day + " " + month + " " + year);	
 		
-		
-		
-
 	}
 
 }
