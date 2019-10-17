fc6f6bc2eea4359eb4d9e2e2df3a939bd564b6e2
byCM <43051181+byCM@users.noreply.github.com>
2019-10-16 20:40:57 -0800

    Create City.java

---
+++ Assignments/A4/City.java
@@ -0,0 +1,30 @@
+
+public class City {
+	private String name;
+	private double latitude;
+	private double longitude;
+
+	public double usercity() {
+		return latitude;
+	}
+	
+	City() {
+		name = "Anchorage";
+		latitude = 61.1912556;
+		longitude = -149.8238687;
+	}
+	
+	
+	City (double B) {
+		latitude = B;
+	}
+
+	public static void main(String[] args) {
+		
+		City input = new City (343.34);
+		System.out.println("You" + input.usercity());
+
+
+	}
+
+}
