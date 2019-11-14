061dab77de58a3f833d15f15b1baaa217403b84d
byCM <43051181+byCM@users.noreply.github.com>
2019-11-14 11:11:36 -0900

    Create Song.java

---
+++ Assignments/A5/Song.java
@@ -0,0 +1,19 @@
+
+public class Song {
+	private String songName;
+	int amount = 200;
+	int[] songArray = new int[amount];
+	
+	public Song(String songName) {
+		songName = "None";
+	}
+	
+	public void addNote() {
+		
+	}
+
+	public static void main(String[] args) {
+		Song notes = new Song("Song one");
+	}
+
+}
