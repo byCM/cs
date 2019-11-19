b6bf7a2d1b8fd2f6f7ef81041ced01e18d25e5d5
byCM <43051181+byCM@users.noreply.github.com>
2019-11-19 14:03:34 -0900

    Update MusicMain.java

--- Assignments/A5/MusicMain.java
+++ Assignments/A5/MusicMain.java
@@ -25,31 +25,66 @@ public class MusicMain {
 		   channels = synth.getChannels();
 		   
 		   	   
-		Note song[] = new Note[200];
-		song[0] = new Note(88, 127, 200); //BASE NOTE
-		song[1] = new Note(93, 127, 130);
-		song[2] = new Note(88, 127, 130);
-		song[3] = new Note(95, 127, 130);
-		song[4] = new Note(88, 127, 130);
-		song[5] = new Note(91, 127, 130);
-		song[6] = new Note(93, 127, 130);
-		song[7] = new Note(88, 127, 130);
-		song[8] = new Note(96, 127, 130);
-		song[9] = new Note(88, 127, 130);
-		song[10] = new Note(97, 127, 130);
-		song[11] = new Note(88, 127, 130);
-		song[12] = new Note(95, 127, 130);
-		song[13] = new Note(96, 127, 130);
+		Note song[] = new Note[200];	
 		
+	
+		song[0] = new Note(87, 127, 150);
+		song[1] = new Note(92, 127, 150);
+		song[2] = new Note(87, 127, 150);
+		song[3] = new Note(94, 127, 150);
+		song[4] = new Note(87, 127, 150);
+		song[5] = new Note(90, 127, 150);
+		song[6] = new Note(92, 127, 150);
+		song[7] = new Note(87, 127, 150);
+		song[8] = new Note(95, 127, 150);
+		song[9] = new Note(87, 127, 150);
+		song[10] = new Note(97, 127, 150);
+		song[11] = new Note(87, 127, 150);
+		song[12] = new Note(94, 127, 150);
+		song[13] = new Note(95, 127, 150);
+		
+		song[14] = new Note(87, 127, 150); //BASE NOTE
+		song[15] = new Note(92, 127, 150);
+		song[16] = new Note(87, 127, 150);
+		song[17] = new Note(94, 127, 150);
+		song[18] = new Note(87, 127, 150);
+		song[19] = new Note(90, 127, 150);
+		song[20] = new Note(92, 127, 150);
+		song[21] = new Note(87, 127, 150);
+		song[22] = new Note(95, 127, 150);
+		song[23] = new Note(87, 127, 150);
+		song[24] = new Note(97, 127, 150);
+		song[25] = new Note(87, 127, 150);
+		song[26] = new Note(94, 127, 150);
+		song[27] = new Note(95, 127, 150);
+
+		
+		song[28] = new Note(67, 127, 125); //BASE NOTE
+		song[29] = new Note(72, 127, 125);
+		song[30] = new Note(67, 127, 125);
+		song[31] = new Note(74, 127, 125);
+		song[32] = new Note(67, 127, 125);
+		song[33] = new Note(70, 127, 125);
+		song[34] = new Note(72, 127, 125);
+		song[35] = new Note(67, 127, 125);
+		song[36] = new Note(75, 127, 125);
+		song[37] = new Note(67, 127, 125);
+		song[38] = new Note(77, 127, 125);
+		song[39] = new Note(67, 127, 125);
+		song[40] = new Note(74, 127, 125);
+		song[41] = new Note(75, 127, 125);
 
 		
 		Song song1 = new Song("Twinkly little star");
 		
-		for (int i = 0; i < 14; i++) {
+		for (int i = 0; i < 42; i++) {
 			song1.addNote(song[i].getValue(), song[i].getVelocity(), song[i].getDuration());
 		}
 		
+		
 		song1.playSong();
+		
+		
 		
 
 		
