445b2e6e3ae99c9d311335ceef5450f0590a1815
byCM <43051181+byCM@users.noreply.github.com>
2019-11-20 12:22:14 -0900

    Update Song.java

--- Assignments/A5/Song.java
+++ Assignments/A5/Song.java
@@ -35,7 +35,16 @@ public class Song {
 		notes.add(details);
 	}
 	
-public void playSong() {
+	/*****************************************************************
+	*
+	* Name: playSong
+	* Description: Main play song method
+	* Returned value: Plays the song with PlayNoteWait
+	* Preconditions: (int, int, int)
+	*
+	*****************************************************************/
+	
+	public void playSong() {
 	
 	   try {
 	      synth = MidiSystem.getSynthesizer();
@@ -56,87 +65,75 @@ public void playSong() {
 	   for(int i=0; i < notes.size(); i++) {
 		   playNoteWait(this.notes.get(i).getValue(), this.notes.get(i).getVelocity(), this.notes.get(i).getDuration());
 	   }
+
 }
+	
+	/*****************************************************************
+	*
+	* Name: playChord
+	* Description: Plays chords using PlayNoteNoWait method
+	* Returned value: Play the chords
+	* Preconditions: (int, int, int)
+	*
+	*****************************************************************/
+	
+	public void playChord() {
+		
+		   try {
+		      synth = MidiSystem.getSynthesizer();
+		      synth.open();
+		   }
+		   catch (MidiUnavailableException e)
+		   {
+		       System.out.println("MIDI error.");
+		       e.printStackTrace();
+		       System.exit(1);
+		   }
+		   // Array for musical instruments / channels is set to the default channels
+		   // and the default instrument is in channels[0]
+		   channels = synth.getChannels();
+		   
+		   System.out.println(this.getName());
+		   
+		   for(int i=0; i < notes.size(); i++) {
+			   playNoteNoWait(this.notes.get(i).getValue(), this.notes.get(i).getVelocity(), this.notes.get(i).getDuration());
+		   }
 
-
-
-
-
+	}
 	
 	
-
-/*
-public static void main(String[] args) {
 	
-	Synthesizer synth = null;
+    public static void playNoteWait(int note, int velocity, int duration) {
+	   //start playing the note using instrument in slot 0
+	   channels[0].noteOn(note, velocity);
+	   //wait out the duration
+	   try
+	   {
+		   Thread.sleep(duration);
+	   }
+	   catch (InterruptedException e)
+	   {
+       System.out.println("Interrupt error.");
+   }
+	   //stop playing the note
+	   channels[0].noteOff(note);
+
+
+	   Synthesizer synth = null;
 	   try {
-	      synth = MidiSystem.getSynthesizer();
-	      synth.open();
+		   synth = MidiSystem.getSynthesizer();
+		   synth.open();
 	   }
 	   catch (MidiUnavailableException e)
 	   {
-	       System.out.println("MIDI error.");
-	       e.printStackTrace();
-	       System.exit(1);
+		   System.out.println("MIDI error.");
+		   e.printStackTrace();
+		   System.exit(1);
 	   }
 	   // Array for musical instruments / channels is set to the default channels
 	   // and the default instrument is in channels[0]
 	   channels = synth.getChannels();
-	   
-	Song song1 = new Song("Twinkly little star");
-	song1.addNote(60, 127, 500);
-	song1.addNote(60, 127, 500);
-	song1.addNote(60, 127, 500);
-	song1.addNote(60, 127, 500);
-	song1.playNoteWait(song1.notes.get(0).getValue(), song1.notes.get(0).getVelocity(), song1.notes.get(0).getDuration());
-	
-	
-	
-}
-*/
-
-
-	
-	
-	
-
-
-	
-	
-	
-	
-public static void playNoteWait(int note, int velocity, int duration)
-{
-   //start playing the note using instrument in slot 0
-   channels[0].noteOn(note, velocity);
-   //wait out the duration
-   try
-   {
-         Thread.sleep(duration);
-   }
-   catch (InterruptedException e)
-   {
-       System.out.println("Interrupt error.");
-   }
-   //stop playing the note
-   channels[0].noteOff(note);
-
-
-Synthesizer synth = null;
-try {
-   synth = MidiSystem.getSynthesizer();
-   synth.open();
-}
-catch (MidiUnavailableException e)
-{
-    System.out.println("MIDI error.");
-    e.printStackTrace();
-    System.exit(1);
-}
-// Array for musical instruments / channels is set to the default channels
-// and the default instrument is in channels[0]
-channels = synth.getChannels();
-}
+    }
 
 
 	 public static void playNoteNoWait(int note, int velocity,int duration)
