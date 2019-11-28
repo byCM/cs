8ecc48a91baffffac190ea7e85ebeecd359bbd11
byCM <43051181+byCM@users.noreply.github.com>
2019-11-27 19:13:45 -0900

    Update Passage.java

--- Assignments/A6/Passage.java
+++ Assignments/A6/Passage.java
@@ -21,6 +21,7 @@ public class Passage {
 	}
 	
 	public static void roomA(char direction) {
+		System.out.println("Room A");
 		System.out.println("You awaken in a maze of twisty little passages, all alike");
 		System.out.println("You can go: South East");
 		
@@ -28,53 +29,125 @@ public class Passage {
 		direction = keyboard.next().charAt(0);
 		
 		if (direction == 's') {
-			roomC();
+			roomB(direction);
 		} else if (direction == 'e') {
-			roomB();
+			roomC(direction);
 		}
 	}
 
-	public static void roomB() {
-		System.out.println("You awaken in a maze of twisty little passages, all alike");
-		System.out.println("You can go: North South East");
-	}
-	
-	public static void roomC() {
+	public static void roomB(char direction) {
+		System.out.println("Room B");
 		System.out.println("You awaken in a maze of twisty little passages, all alike");
 		System.out.println("You can go: North South East");
 		
+		Scanner keyboard = new Scanner(System.in);
+		direction = keyboard.next().charAt(0);
+		
+		if (direction == 'n') {
+			roomA(direction);
+		} else if (direction == 's') {
+			roomE(direction);
+		} else if (direction == 'e') {
+			roomC(direction);
+		}
 	}
 	
-	public void roomF(char direction) {
+	public static void roomC(char direction) {
+		System.out.println("Room C");
+		System.out.println("You awaken in a maze of twisty little passages, all alike");
+		System.out.println("You can go: North South East");
+		
+		Scanner keyboard = new Scanner(System.in);
+		direction = keyboard.next().charAt(0);
+		
+		if (direction == 'n') {
+			roomA(direction);
+		} else if (direction == 's') {
+			roomB(direction);
+		} else if (direction == 'e') {
+			roomD(direction);
+		}
+		
+	}
+	
+	public static void roomF(char direction) {
 		System.out.println("Game over, you are in the Wumpuses lair");
 		
 	}
 	
-	public void roomD(char direction) {
+	public static void roomD(char direction) {
+		System.out.println("Room D");
 		System.out.println("You awaken in a maze of twisty little passages, all alike");
 		System.out.println("You can go: North South East West");
 		
-	}
-	
-	public void roomE(char direction) {
-		System.out.println("You awaken in a maze of twisty little passages, all alike");
-		System.out.println("You can go: North East West");
+		Scanner keyboard = new Scanner(System.in);
+		direction = keyboard.next().charAt(0);
+		
+		if (direction == 'n') {
+			roomF(direction);
+		} else if (direction == 's') {
+			roomE(direction);
+		} else if (direction == 'e') {
+			roomG(direction);
+		} else if (direction == 'w') {
+			roomC(direction);
+		}
 		
 	}
 	
-	public void roomG(char direction) {
+	public static void roomE(char direction) {
+		System.out.println("Room E");
+		System.out.println("You awaken in a maze of twisty little passages, all alike");
+		System.out.println("You can go: North East West");
+		
+		Scanner keyboard = new Scanner(System.in);
+		direction = keyboard.next().charAt(0);
+		
+		if (direction == 'n') {
+			roomD(direction);
+		} else if (direction == 'w') {
+			roomB(direction);
+		} else if (direction == 'e') {
+			roomH(direction);
+		}
+	}
+	
+	public static void roomG(char direction) {
+		System.out.println("Room G");
 		System.out.println("You awaken in a maze of twisty little passages, all alike");
 		System.out.println("You can go: South West");
 		
-	}
-	
-	public void roomH(char direction) {
-		System.out.println("You awaken in a maze of twisty little passages, all alike");
-		System.out.println("You can go: North East West");
+		Scanner keyboard = new Scanner(System.in);
+		direction = keyboard.next().charAt(0);
+		
+		if (direction == 's') {
+			roomH(direction);
+		} else if (direction == 'w') {
+			roomD(direction);
+		}
 		
 	}
 	
-	public void roomI(char direction) {
+	public static void roomH(char direction) {
+		System.out.println("Room H");
+		System.out.println("You awaken in a maze of twisty little passages, all alike");
+		System.out.println("You can go: North East West");
+		
+		Scanner keyboard = new Scanner(System.in);
+		direction = keyboard.next().charAt(0);
+		
+		if (direction == 'n') {
+			roomG(direction);
+		} else if (direction == 'w') {
+			roomD(direction);
+		} else if (direction == 'e') {
+			roomI(direction);
+		}
+		
+		
+	}
+	
+	public static void roomI(char direction) {
 		System.out.println("You made it out alive!");
 		
 	}
