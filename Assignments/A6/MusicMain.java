158e25d4b308ac1bd2872c9c256c8b7617eed2a0
byCM <43051181+byCM@users.noreply.github.com>
2019-11-27 23:15:08 -0900

    Create MusicMain.java

---
+++ Assignments/A6/MusicMain.java
@@ -0,0 +1,131 @@
+import java.io.*;
+import java.util.*;
+
+import javax.sound.midi.MidiChannel;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Synthesizer;
+
+public class MusicMain {
+	public Scanner x;
+	
+	private static MidiChannel channels[] = null;
+	
+	public void openFile() {
+		try { 
+			x = new Scanner(new File("song.txt"));
+		}
+		catch (Exception e) {
+			System.out.println("Could Not Find File");
+		}
+	}
+	
+	public void readFile() {
+		while (x.hasNext()) {
+			
+			int temp = 50;
+				
+			String a = x.next();
+			int b = x.nextInt();
+			int c = x.nextInt();
+			
+			if (a.equals("C")) {
+				temp = 60;
+			} else if (a.equals("C#")) {
+				temp = 61;
+			} else if (a.equals("D")) {
+				temp = 62;
+			} else if (a.equals("D#")) {
+				temp = 63;
+			} else if (a.equals("E")) {
+				temp = 64;
+			} else if (a.equals("F")) {
+				temp = 65;
+			} else if (a.equals("F#")) {
+				temp = 66;
+			} else if (a.equals("G")) {
+				temp = 67;
+			} else if (a.equals("AFlat")) {
+				temp = 68;
+			} else if (a.equals("A")) {
+				temp = 69;
+			} else if (a.equals("BFlat")) {
+				temp = 70;
+			} else if (a.equals("B")) {
+				temp = 71;
+			}
+			
+			Note song[] = new Note[200];
+			song[0] = new Note(temp, b, c);
+			
+			Song songOne = new Song("Exorcist Theme Song");
+	    	for (int i = 0; i < 1; i++) {
+				songOne.addNote(song[i].getValue(), song[i].getVelocity(), song[i].getDuration());
+				System.out.println(song[i].getValue());
+				System.out.println(song[i].getVelocity());
+				System.out.println(song[i].getDuration());
+			}
+	    	
+	    	songOne.playSong();
+	    	
+		}
+	}
+	
+	public void closeFile() {
+		x.close();
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		Synthesizer synth = null;
+		   try {
+		      synth = MidiSystem.getSynthesizer();
+		      synth.open();
+		   }
+		   catch (MidiUnavailableException e)
+		   {
+		       System.out.println("MIDI error.");
+		       e.printStackTrace();
+		       System.exit(1);
+		   }
+		   // Array for musical instruments / channels is set to the default channels
+		   // and the default instrument is in channels[0]
+		   channels = synth.getChannels();
+		   
+		   MusicMain reader = new MusicMain();
+		   reader.openFile();
+		   reader.readFile();
+		   reader.closeFile();
+	
+		   
+		    
+		/* // Three parts of the theme  
+		   
+		Note song[] = new Note[200];	
+		
+
+		
+		//Part 1 instrumental
+		song[0] = new Note(0, 0, 200);
+		song[1] = new Note(0, 0, 200);
+		song[2] = new Note(0, 0, 200);
+		
+		
+
+		
+		
+		Song songOne = new Song("Exorcist Theme Song");
+    	for (int i = 0; i < 89; i++) {
+			songOne.addNote(song[i].getValue(), song[i].getVelocity(), song[i].getDuration());
+		}
+    	songOne.playSong();
+    	*/
+
+
+		
+		
+		
+	}
+
+}
