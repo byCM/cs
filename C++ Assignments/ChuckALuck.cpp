af2d020d2fd301f45a6d8a577bf0f62d71bc9097
byCM <43051181+byCM@users.noreply.github.com>
2020-01-22 13:24:30 -0900

    Create ChuckALuck.cpp

---
+++ C++ Assignments/ChuckALuck.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    // Wager amount
+    int wager;
+
+    // Dice Roll
+    int n;
+
+    cout << "Please enter a number from 1-6:" << endl;
+
+    cout << "Please enter a number from 1-6:" << endl;
+
+    return 0;
+}
