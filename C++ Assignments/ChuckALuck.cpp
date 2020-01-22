1845e339ec9afc1430b11a9ea216887b8060b31e
byCM <43051181+byCM@users.noreply.github.com>
2020-01-22 13:39:40 -0900

    Update ChuckALuck.cpp

--- C++ Assignments/ChuckALuck.cpp
+++ C++ Assignments/ChuckALuck.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<time.h>
 using namespace std;
 
 
@@ -10,9 +11,23 @@ int main()
     // Dice Roll
     int n;
 
-    cout << "Please enter a number from 1-6:" << endl;
+    int first, second, third;
+
+    cout << "Please enter your wager:" << endl;
+    cin >> wager;
 
     cout << "Please enter a number from 1-6:" << endl;
+    cin >> n;
 
+    srand(time(NULL));
+    first = rand() % (6 - 1 + 1) + 1;
+    second = (rand() % 6) + 1;
+    third = (rand() % 6) + 1;
+
+    cout << "roll " << first << endl;
+    cout << "roll " << second << endl;
+    cout << "roll " << third << endl;
+
+ 
     return 0;
 }
