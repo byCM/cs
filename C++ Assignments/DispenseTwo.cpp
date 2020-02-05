d02b8b8a1ce3b6f89a1bbfdc7e937aecff95dbe1
byCM <43051181+byCM@users.noreply.github.com>
2020-02-05 10:24:30 -0900

    Create DispenseTwo.cpp

---
+++ C++ Assignments/DispenseTwo.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+
+void value(int n, int &number) {
+    cout << "Total Dollars to dispense: $" << n << endl;
+
+    n = number / 100;
+    cout << "$100 bills = " << n << '\n';
+
+    n = (number % 100) / 50;
+    cout << "$50 bills = " << n << '\n';
+
+    n = ((number % 100) % 50) / 20;
+    cout << "$20 bills = " << n << '\n';
+
+    n = (((number % 100) % 50) % 20) / 10;
+    cout << "$10 bills = " << n << '\n';
+
+    n = ((((number % 100) % 50) % 20) % 10) / 5;
+    cout << "$5 bills = " << n << '\n';
+
+    n = (((((number % 100) % 50) % 20) % 10) % 5) / 2;
+    cout << "$2 Bills = " << n << '\n';
+
+    n = ((((((number % 100) % 50) % 20) % 10) % 5) % 2) / 1;
+    cout << "$1 Bills = " << n << '\n';
+}
+
+
+
+int main()
+{
+    int n;
+
+    cout << "Please enter a integer 1-999" << endl;
+    cin >> n;
+
+    while (n < 1 | n > 999) {
+        cout << "Please enter a integer 1-999" << endl;
+        cin >> n;
+    }
+
+    int number = n;
+    value(n, number);
+
+
+    return 0;
+}
