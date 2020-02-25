ac8b62d8c7c620c61575e5562a435c6d3d3080c0
Michael Ross <43051181+byCM@users.noreply.github.com>
2020-02-24 16:57:38 -0900

    Update decrypttest.cpp

--- C++ Assignments/decrypttest.cpp
+++ C++ Assignments/decrypttest.cpp
@@ -3,34 +3,66 @@
 using namespace std;
 
 
-
 void decrypt(int key) {
 
-	char message[] = "HI THERE";
+	char message[] = ",vtaNm a_\"dabp!!";
+	//char message[] = "HI THERE";
 
 	int one = (message[3] << 24) | (message[2] << 16) | (message[1] << 8) | (message[0]);
-
 	int two = (message[7] << 24) | (message[6] << 16) | (message[5] << 8) | (message[4]);
+	int three = (message[11] << 24) | (message[10] << 16) | (message[9] << 8) | (message[8]);
+	int four = (message[15] << 24) | (message[14] << 16) | (message[13] << 8) | (message[12]);
 
-	int ff = one + key;
+	int first = one + key;
+	int second = two + key;
+	int third = three + key;
+	int fourth = four + key;
+
 
 	
-	message[3] = (char)(ff >> 24); 
-	message[2] = (char)(ff >> 16); 
-	message[1] = (char)(ff >> 8); 
-	message[0] = (char)ff;
+	message[3] = (char)(first >> 24); 
+	message[2] = (char)(first >> 16); 
+	message[1] = (char)(first >> 8); 
+	message[0] = (char)first;
 
-	cout << message[0] << message[1] << message[2] << message[3] <<endl;
+	message[7] = (char)(second >> 24);
+	message[6] = (char)(second >> 16);
+	message[5] = (char)(second >> 8);
+	message[4] = (char)second;
+	
+	message[11] = (char)(third >> 24);
+	message[10] = (char)(third >> 16);
+	message[9] = (char)(third >> 8);
+	message[8] = (char)third;
+
+	message[15] = (char)(fourth >> 24);
+	message[14] = (char)(fourth >> 16);
+	message[13] = (char)(fourth >> 8);
+	message[12] = (char)fourth;
+	
+
+	cout << message[0] << message[1] << message[2] << message[3] << message[4] << message[5] << message[6] << message[7] <<
+		message[8] << message[9] << message[10] << message[11] << message[12] << message[13] << message[14] << message[15] << endl;
+
+
+
+	/*
+	for (int i = 0; i < 15; i++) {
+		cout << message[i];
+	}
+	*/
+
 
 }
 
 int main() {
 
-	//char message[] = ", vtaNm a_\"dabp!!";
-
-	decrypt(2);
-
-
+	//char message[] = ", vtaNm a_\"dabp
+	
+	
+	for (int i = 0; i < 501; i++) {
+		decrypt(i);
+	}
 
 
 
