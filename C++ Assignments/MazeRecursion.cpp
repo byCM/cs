4d2825dd883733eb1697b107aeca4a9080028ffd
Michael Ross <43051181+byCM@users.noreply.github.com>
2020-03-02 11:08:13 -0900

    Update MazeRecursion.cpp

--- C++ Assignments/MazeRecursion.cpp
+++ C++ Assignments/MazeRecursion.cpp
@@ -8,8 +8,8 @@ using namespace std;
 const int WIDTH = 40;
 const int HEIGHT = 40;
 
-
-void locate(char maze[1][39]) {
+//1 and 39
+void locate(char maze[][WIDTH]) {
 
 	if (WIDTH <= 2 || HEIGHT <= 2) {
 		return;
@@ -22,28 +22,34 @@ void locate(char maze[1][39]) {
 
 	for (int x = 0; x < WIDTH; x++) {
 		maze[l][x] = 'X'; 
+		cout << maze[l][x];
 	}
 
 	// random hole in location across 
 	int q = 0;
 	q = rand();
-	maze[l][q] = ' ';
+	//maze[l][q] = ' ';
+
+
+
 
 }
 
-void printMaze(char maze[][WIDTH], int curx, int cury)
+void printMaze(char maze[][WIDTH])
 {
+	locate(maze);
+
 	for (int y = 0; y < HEIGHT; y++)
 	{
 		for (int x = 0; x < WIDTH; x++)
 		{
-			if ((x == curx) && (y == cury))
-				cout << "@";
-			else
 				cout << maze[y][x];
 		}
 		cout << endl;
 	}
+
+
+
 }
 
 
@@ -59,17 +65,14 @@ int main() {
 			maze[x][y] = ' ';
 
 	for (x = 0; x < WIDTH; x++) {
-		maze[0][x] = 'X';//TOP
-		maze[x][0] = 'X';//LEFT
-		maze[HEIGHT - 1][x] = 'X';//BOTTOM
-		maze[x][WIDTH - 1] = 'X';//RIGHT
+		maze[0][x] = 'X';//top 
+		maze[x][0] = 'X';//left side
+		maze[HEIGHT - 1][x] = 'X';//bottom
+		maze[x][WIDTH - 1] = 'X';//right side
 
 	}
 
 
-	int path[1500][2];
-	path[0][0] = 0;
-
-	printMaze(maze, path[1][0], path[0][1]);
+	printMaze(maze);
 }
 
