9d1fafd7bd1e2d0541e215b2b1f1b0f3e1350165
Michael Ross <43051181+byCM@users.noreply.github.com>
2020-03-04 10:21:49 -0900

    Update MazeRecursion.cpp

--- C++ Assignments/MazeRecursion.cpp
+++ C++ Assignments/MazeRecursion.cpp
@@ -9,27 +9,24 @@ const int WIDTH = 40;
 const int HEIGHT = 40;
 
 //1 and 39
-void locate(char maze[][WIDTH]) {
+void locate(char maze[][WIDTH], int upperLeft, int upperRight, int bottomLeft, int bottomRight) {
 
 	if (WIDTH <= 2 || HEIGHT <= 2) {
 		return;
 	}
 
-	//randon x across
-
-
 	srand(time(NULL));
 	//MAIN RANDOM LINE DOWN SIDE
-	int l = rand() % HEIGHT - 2;
-	int q = rand() % WIDTH - 2;
-	int k = rand() % WIDTH - 2;
+	int l = rand() % (HEIGHT - 1) + 1;
+	int q = rand() % (WIDTH - 1) + 1;
+	int k = rand() % (HEIGHT - 1) + 1;
 
-	for (int x = 0; x < HEIGHT - 1; x++) {
+	for (int x = 0; x < 39; x++) {
 		maze[l][x] = 'X';
 		maze[l][q] = ' ';
 	}
 
-	for (int x = 0; x < l; x++) {
+	for (int x = l; x < HEIGHT; x++) {
 		maze[x][l] = 'X';
 	}
 
@@ -42,7 +39,7 @@ void locate(char maze[][WIDTH]) {
 
 void printMaze(char maze[][WIDTH])
 {
-	locate(maze);
+	locate(maze, 1, 1, 39, 39);
 
 	for (int y = 0; y < HEIGHT; y++)
 	{
