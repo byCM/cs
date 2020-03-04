730971b20399b87605fcdd4bd94a9466e8d449a7
Michael Ross <43051181+byCM@users.noreply.github.com>
2020-03-04 10:03:51 -0900

    Update decrypttest.cpp

--- C++ Assignments/decrypttest.cpp
+++ C++ Assignments/decrypttest.cpp
@@ -10,23 +10,27 @@ void decrypt(int key) {
 	// This includes both ways to solve this, one being very lengthy and the other being very simple using pointers. 
 
 
-	/*
+
 	int one = (int(message[3]) << 24) | (int(message[2]) << 16) | (int(message[1]) << 8) | (int(message[0]));
 	int two = (int(message[7]) << 24) | (int(message[6]) << 16) | (int(message[5]) << 8) | (int(message[4]));
 	int three = (int(message[11]) << 24) | (int(message[10]) << 16) | (int(message[9]) << 8) | (int(message[8]));
 	int four = (int(message[15]) << 24) | (int(message[14]) << 16) | (int(message[13]) << 8) | (int(message[12]));
+
 	message[3] = (char)(one - key >> 24);
 	message[2] = (char)(one - key >> 16);
 	message[1] = (char)(one - key >> 8);
 	message[0] = (char)one - key;
+
 	message[7] = (char)(two - key >> 24);
 	message[6] = (char)(two - key >> 16);
 	message[5] = (char)(two - key >> 8);
 	message[4] = (char)two - key;
+
 	message[11] = (char)(three - key >> 24);
 	message[10] = (char)(three - key >> 16);
 	message[9] = (char)(three - key >> 8);
 	message[8] = (char)three - key;
+
 	message[15] = (char)(four - key >> 24);
 	message[14] = (char)(four - key >> 16);
 	message[13] = (char)(four - key >> 8);
@@ -35,8 +39,10 @@ void decrypt(int key) {
 	for (int i = 0; i < 16; i++) {
 		cout << message[i];
 	}
-	*/
+}
 
+
+	/*
 	int i = 0;
 
 	do {
@@ -48,6 +54,7 @@ void decrypt(int key) {
 	cout << message << endl;
 
 }
+*/
 
 
 int main() {
