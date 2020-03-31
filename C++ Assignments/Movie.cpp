85e63f5d97e30e3105463475b4b587de1487ed11
Michael Ross <43051181+byCM@users.noreply.github.com>
2020-03-30 21:27:03 -0800

    Create Movie.cpp

---
+++ C++ Assignments/Movie.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+class Movie
+{
+private:
+    string name;
+    string rating;
+public:
+    Movie() {}
+    Movie(string name1, string rating1) {
+        name = name1;
+        rating = rating1;
+    }
+    void setName(string s) {
+        name = s;
+    }
+    void setRating(string r) {
+        rating = r;
+    }
+    string getName() {
+        return name;
+    }
+    string getRating() {
+        return rating;
+    }
+};
+
+void sortByName(Movie movies[], int n);
+
+int main() {
+    Movie movies[6];
+    movies[0] = Movie("Black Panther", "PG-13");
+    movies[1] = Movie("Avengers: Infinity War", "PG-13");
+    movies[2] = Movie("A Wrinkle In Time", "PG");
+    movies[3] = Movie("Ready Player One", "PG-13");
+    movies[4] = Movie("Red Sparrow", "R");
+    movies[5] = Movie("The Incredibles 2", "G");
+
+    sortByName(movies, 6);
+    //display sorted data
+    cout << "Movies sorted by name are " << endl;
+    for (int i = 0; i < 6; i++) {
+        cout << left << setw(25) << movies[i].getName() << setw(10) << movies[i].getRating() << endl;
+    }
+    return 0;
+}
+
+void sortByName(Movie movies[], int n) {
+    //bubble sort
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (movies[j].getName() > movies[j + 1].getName()) {
+                Movie temp = movies[j];
+                movies[j] = movies[j + 1];
+                movies[j + 1] = temp;
+            }
+        }
+    }
+}
+
