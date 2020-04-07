b6a694426a6dc1456f706633fa90055e2b7a4610
Michael Ross <43051181+byCM@users.noreply.github.com>
2020-04-07 12:26:19 -0800

    Update EnrollmentsV2.cpp

--- C++ Assignments/EnrollmentsV2.cpp
+++ C++ Assignments/EnrollmentsV2.cpp
@@ -11,60 +11,54 @@ int main()
     int j = 0, k = 0, l = 0, m = 0, n = 0, o = 0, p = 0, q = 0, r = 0, t = 0;
     char size;
 
-
     ifstream myfile;
 
     myfile.open("numbers.txt");
 
-    while (!myfile.eof()) {
-        getline(cin, s);
-        cin >> size;
+    if (myfile.is_open()) {
+        while (getline(cin, s)) {
+            cin >> size;
 
-        switch (size) {
-        case '1':
-            j++;
-            break;
-        case '2':
-            k++;
-            break;
-        case '3':
-            l++;
-            break;
-        case '4':
-            m++;
-            break;
-        case '5':
-            n++;
-            break;
-        case '6':
-            o++;
-            break;
-        case '7':
-            p++;
-            break;
-        case '8':
-            q++;
-            break;
-        case '9':
-            r++;
-            break;
-        default:
-            t++;
-            break;
+            switch (size) {
+            case '1':
+                j++;
+                break;
+            case '2':
+                k++;
+                break;
+            case '3':
+                l++;
+                break;
+            case '4':
+                m++;
+                break;
+            case '5':
+                n++;
+                break;
+            case '6':
+                o++;
+                break;
+            case '7':
+                p++;
+                break;
+            case '8':
+                q++;
+                break;
+            case '9':
+                r++;
+                break;
+            default:
+                t++;
+                break;
 
+            }
         }
+        myfile.close();
     }
- 
+    else {
+        cout << "numbers.txt does not exists!" << endl;
+    }
+    return 0;
+    
+
 }
-
-
-
-
-    
-
-
-    
-
-   
-
-
