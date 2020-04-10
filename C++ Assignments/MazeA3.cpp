87285254e200f31049c5e928e9c8fb05ddc69642
Michael Ross <43051181+byCM@users.noreply.github.com>
2020-04-09 22:53:42 -0800

    Update MazeA3.cpp
    remove comments

--- C++ Assignments/MazeA3.cpp
+++ C++ Assignments/MazeA3.cpp
@@ -1,9 +1,5 @@
-// maze-solver.cpp
-//
-// This program fills in a maze with random positions and then runs the solver to solve it.
-// The moves are saved in two arrays, which store the X/Y coordinates we are moving to.
-// They are output in main in forward order.
-//
+//mazeSolver.cpp
+
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
@@ -13,23 +9,18 @@ using namespace std;
 const int WIDTH = 20;
 const int HEIGHT = 20;
 
-// Function prototypes
+
 void printMaze(char maze[][WIDTH], int curx, int cury);
 bool validMove(char maze[][WIDTH], bool visited[][WIDTH],
                int newX, int newY);
 bool move(char maze[][WIDTH], bool visited[][WIDTH],
           int &curX, int &curY, int newX, int newY);
-// *** search has new parameters to remember the coordinates of the solution we find
 bool search(char maze[][WIDTH], bool visited[][WIDTH], int x, int y, 
  	    int solutionX[], int solutionY[], int &numEntries);
-// *** New function, see below
 void addToArrays(int x[], int y[], int &numEntries, int xVal, int yVal);
 
 
 
-// This new function adds two numbers to the arrays and increments the count of how many
-// numbers have been added. It assumes the arrays have been created big enough to not
-// have overflow. It is used to remember the coordinates of our solution.
 void addToArrays(int x[], int y[], int &numEntries, int xVal, int yVal)
 {
    x[numEntries] = xVal;
@@ -56,11 +47,6 @@ bool validMove(char maze[][WIDTH], bool visited[][WIDTH],
  return true;
 }
 
-// Make the move on the maze to move to a new coordinate
-// I passed curX and curY by reference so they are changed to
-// the new coordinates.  Here we assume the move coordinates are valid.
-// This returns true if we move onto the exit, false otherwise.
-// Also update the visited array.
 bool move(char maze[][WIDTH], bool visited[][WIDTH],
 	  int &curX, int &curY, int newX, int newY)
 {
@@ -74,7 +60,6 @@ bool move(char maze[][WIDTH], bool visited[][WIDTH],
   return foundExit;
 }
 
-// Display the maze in ASCII
 void printMaze(char maze[][WIDTH], int curx, int cury)
 {
   for (int y=0; y < HEIGHT;y++)
@@ -184,11 +169,7 @@ int main()
         visited[y][x] = false;
  visited[y][x] = true;
 
- // Here I created arrays to store the x/y coordinates for the path of our solution.
- // The array is of size [HEIGHT-2]*[WIDTH-2] since we'll never exceed that size.
- // I also made a variable to count how many entries we make..
- // It would probably be more convenient to make a class to store this data rather than
- // have two separate arrays!
+	
  int solutionX[(HEIGHT-2)*(WIDTH-2)], solutionY[(HEIGHT-2)*(WIDTH-2)];
  int numPoints = 0;
 
