e24622a27abef3a8f67b501b68f51343377f48fe
Michael Ross <43051181+byCM@users.noreply.github.com>
2020-04-10 10:06:28 -0800

    Update MazeA3.cpp

--- C++ Assignments/MazeA3.cpp
+++ C++ Assignments/MazeA3.cpp
@@ -1,188 +1,267 @@
-//mazeSolver.cpp
+#include<iostream>
 
-#include <iostream>
-#include <ctime>
-#include <cstdlib>
-using namespace std;
+#include<ctime>
 
-// ************ Width and Height changed to 20x20  ********************
-const int WIDTH = 20;
-const int HEIGHT = 20;
+#include<stdlib>
 
+class maze
 
-void printMaze(char maze[][WIDTH], int curx, int cury);
-bool validMove(char maze[][WIDTH], bool visited[][WIDTH],
-               int newX, int newY);
-bool move(char maze[][WIDTH], bool visited[][WIDTH],
-          int &curX, int &curY, int newX, int newY);
-bool search(char maze[][WIDTH], bool visited[][WIDTH], int x, int y, 
- 	    int solutionX[], int solutionY[], int &numEntries);
-void addToArrays(int x[], int y[], int &numEntries, int xVal, int yVal);
-
-
-
-void addToArrays(int x[], int y[], int &numEntries, int xVal, int yVal)
 {
-   x[numEntries] = xVal;
-   y[numEntries] = yVal;
-   numEntries++;
+
+public :
+
+maze()
+
+{
+
+const int WIDTH=20;
+
+const int HEIGHT=20;
+
 }
 
-// Return true or false if moving to the specified coordinate is valid
-// Return false if we have been to this cell already
-bool validMove(char maze[][WIDTH], bool visited[][WIDTH],
-               int newX, int newY)
+void print maze(char maze[][WIDTH],int curx,int cury)
+
 {
- // Check for going off the maze edges
- if (newX < 0 || newX >= WIDTH)
-	return false;
- if (newY < 0 || newY >= HEIGHT)
-	return false;
- // Check if target is a wall
- if (maze[newY][newX]=='X')
-	return false;
- // Check if visited
- if (visited[newY][newX])
-	return false;
- return true;
+
+for(int y=0;y<HEIGHT;y++)
+
+{
+
+for(int x=0;x<WIDTH;x++)
+
+{
+
+if((x==curx)&&(y==cury))
+
+cout<<"@";
+
+else
+
+cout<<maze[y][x];
+
 }
 
-bool move(char maze[][WIDTH], bool visited[][WIDTH],
-	  int &curX, int &curY, int newX, int newY)
+cout<<endl;
+
+}}
+
+bool validMove(char maze[][WIDTH],bool visited[][WIDTH],int newX,int newY)
+
 {
-  bool foundExit = false;
-  if (maze[newY][newX]=='E') 	// Check for exit
-	foundExit = true;
-  curX = newX;			// Update location
-  curY = newY;
-  visited[curY][curX] = true;
- 
-  return foundExit;
+
+if(newX<0||newX>=WIDTH)
+
+return false;
+
+if(newY<0||newY>=HEIGHT)
+
+return false;
+
+if(maze[newY][newX]=='X')
+
+return false;
+
+if(visited[newY][newX])
+
+return false;
+
+return true;
+
 }
 
-void printMaze(char maze[][WIDTH], int curx, int cury)
+bool move(char maze[][WIDTH],bool visited[][WIDTH],int &curX,int &curY,int newX,int newY)
+
 {
-  for (int y=0; y < HEIGHT;y++)
-  {
-    for (int x=0; x < WIDTH; x++)
-    {
-	if ((x==curx) && (y==cury))
-		cout << "@";
-	else
-		cout << maze[y][x];
-    }
-    cout << endl;
-  }
+
+bool foundExit=false;
+
+if(maze[newY][newX]=='E')
+
+foundExit=true;
+
+curX=newX;
+
+curY=newY;
+
+visited[curY][curX]=true;
+
+return foundExit;
+
 }
 
-// Recursively search from x,y until we find the exit
-bool search(char maze[][WIDTH], bool visited[][WIDTH], 
-            int x, int y,
- 	    int solutionX[], int solutionY[], int &numEntries)
+bool search(char maze[][WIDTH],bool visited[][WIDTH],int x,int y,int solutionX[],int solutionY[],int &numEntries)
+
 {
-   bool foundExit;
 
-   if (maze[y][x]=='E')
-	return true;
-   visited[y][x]=true;
-   if (validMove(maze,visited,x,y-1))
-	foundExit = search(maze,visited,x,y-1,solutionX,solutionY,numEntries);
-   if (!foundExit && (validMove(maze,visited,x,y+1)))
-	foundExit = search(maze,visited,x,y+1,solutionX,solutionY,numEntries);
-   if (!foundExit && (validMove(maze,visited,x-1,y)))
-	foundExit = search(maze,visited,x-1,y,solutionX,solutionY,numEntries);
-   if (!foundExit && (validMove(maze,visited,x+1,y)))
-	foundExit = search(maze,visited,x+1,y,solutionX,solutionY,numEntries);
+bool foundExit;
 
-   if (foundExit)
-   {
-	// Remember coordinates we found the exit in the solution arrays
-	addToArrays(solutionX, solutionY, numEntries, x, y);	
-	return true;
-   }
-   return false;
+if(maze[y][x]=='E')
+
+return true;
+
+visited[y][x]=true;
+
+if(validMove(maze,visited,x,y-1))
+
+foundExit=search(maze,visited,x,y-1,solutionX,solutionY,numEntries);
+
+if(!foundExit&&(validMove(maze,visited,x,y+1)))
+
+foundExit=search(maze,visited,x,y+1,solutionX,solutionY,numEntries);
+
+if(!foundExit&&(validMove(maze,visited,x-1,y)))
+
+foundExit=search(maze,visited,x-1,solutionX,solutionY,numEntries);
+
+if(!foundExit&&(validMove(maze,visited,x+1,y)))
+
+foundExit=search(maze,visited,x+1,y,solutionX,solutionY,numEntries);
+
+if(foundExit)
+
+{
+
+AddToArrays(solutionX,solutionY,numEntries,x,y);
+
+return true;
+
+}
+
+return false;
+
+}
+
+void AddToArrays(int x[],int y[],int &numEntries,int xVal,int yVal)
+
+{
+
+x[numEntries]=xVal;
+
+y[numEntries]=yVal;
+
+numEntries++;
+
 }
 
 int main()
+
 {
- char maze[HEIGHT][WIDTH];
- bool visited[HEIGHT][WIDTH];
 
- // Seed random number generator with clock time
- srand(time(NULL));
+maze m;
 
- /****************
-  Programmatically fill out the maze with ***'s on the borders and spaces in the middle
-  ****************/
- // All blank
- for (int x = 0; x < WIDTH; x++)
-  for (int y = 0; y < HEIGHT; y++)
-	maze[y][x] = ' ';
- // Borders with X
- for (int x = 0; x < WIDTH; x++)
- {
-	maze[0][x] = 'X';
-	maze[HEIGHT-1][x] = 'X';
- }
- for (int y = 0; y < HEIGHT; y++)
- {
-	maze[y][0] = 'X';
-	maze[y][WIDTH-1] = 'X';
- }
+char Maze[HEIGHT][WIDTH];
 
- // ***** Randomly fill in 25% of the middle
- int numCells = static_cast<int>((HEIGHT-2)*(WIDTH-2)*0.25);
- int count = 0;
- while (count < numCells)
- {
-    int x = (rand() % (WIDTH-2)) +1;
-    int y = (rand() % (HEIGHT-2)) +1;
-    if (maze[y][x]==' ')
-    {
-	maze[y][x]='X';
-	count++;
-    }
- }
+bool visited[HEIGHT][WIDTH];
 
- // ***** Pick a random start and end that is not a wall *****
- int x = (rand() % (WIDTH-2)) +1;
- int y = (rand() % (HEIGHT-2)) +1;
- while (maze[y][x]=='X')
- {
-   x = (rand() % (WIDTH-2)) +1;
-   y = (rand() % (HEIGHT-2)) +1;
- }
- // At this point, (x,y) contains our start position
- // ***** Pick a random end position that is not a wall *******
- int exitX = (rand() % (WIDTH-2)) +1;
- int exitY = (rand() % (HEIGHT-2)) +1;
- while (maze[exitY][exitX]=='X')
- {
-   exitX = (rand() % (WIDTH-2)) +1;
-   exitY = (rand() % (HEIGHT-2)) +1;
- }
- maze[exitY][exitX]='E';
+srand(time(NULL));
 
- // Initialize visited locations to false
- for (int x = 0; x < WIDTH; x++)
-  for (int y = 0; y < HEIGHT; y++)
-        visited[y][x] = false;
- visited[y][x] = true;
+for(int x=0;x<WIDTH;x++)
 
-	
- int solutionX[(HEIGHT-2)*(WIDTH-2)], solutionY[(HEIGHT-2)*(WIDTH-2)];
- int numPoints = 0;
+for(int y=0;y<HEIGHT;y++)
 
- bool found = search(maze,visited,x,y,solutionX,solutionY,numPoints);
- if (!found)
-	cout << "No solution found.";
- else
- {
-	cout << "Solution found!  Here is the path from the start." << endl;
-	for (int i = numPoints-1; i >= 0; i--)
-	{
-		printMaze(maze, solutionX[i], solutionY[i]);
-		cout << endl;
- 	}
- }
+Maze[y][x]='';
+
+for(int x=0;x<WIDTH;x++)
+
+{
+
+Maze[0][x]='X';
+
+Maze[HEIGH-1T][x]='X';
+
+}
+
+for(int y=0;y<HEIGHT;y++)
+
+{
+
+Maze[y][0]='X';
+
+Maze[y][WIDTH-1]='X';
+
+}
+
+int numcells=static_cast<int>((HEIGHT-2)*(WIDTH-2)*0.25);
+
+int count=0;
+
+while(count<numcells)
+
+{
+
+int x=(rand()%(WIDTH-2))+1;
+
+int y=(rand()%(HEIGHT-2))+1;
+
+if(Maze[y][x]=='')
+
+{
+
+Maze[y][x]='X';
+
+count++;
+
+}}
+
+int x=(rand()%(WIDTH-2))+1;
+
+int y=(rand()%(HEIGHT-2))+1;
+
+}
+
+int exitX=(rand()%(WIDTH-2))+1;
+
+int exitY=(rand()%(HEIGHT-2))+1;
+
+while(Maze[exitY][exitX]=='X')
+
+{
+
+exitX=(rand()%(WIDTH-2))+1;
+
+exitY=(rand()%(HEIGHT-2))+1;
+
+}
+
+Maze[exitY][exitX]='E';
+
+for(int x=0;x<WIDTH;x++)
+
+for(int y=0;y<HEIGHT;y++)
+
+visited[y][x]=false;
+
+visited[y][x]=true;
+
+int solutionX[(HEIGHT-2)*(WIDTH-2)],solutionY[(HEIGHT-2)*(WIDTH-2)];
+
+int numpoints=0;
+
+boolfound=m.search(Maze,visited,x,y,solutionX,solutionY,numpoints);
+
+if(!found)
+
+{
+
+cout<<"no solution found";
+
+else
+
+{
+
+cout<<solution found!here is the path from the start"<<endl;
+
+for(int i=numpoints-1;i>=0;i--)
+
+{
+
+m.printmaze(Maze,solutionX[i],solutionY[i]);
+
+cout<<endl;
+
+}
+
+}
+
 }
