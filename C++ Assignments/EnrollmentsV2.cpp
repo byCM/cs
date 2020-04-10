c10ee465f7b1539f14e2bc40484ece3444bee1b8
Michael Ross <43051181+byCM@users.noreply.github.com>
2020-04-09 23:16:49 -0800

    no multiple variables

--- C++ Assignments/EnrollmentsV2.cpp
+++ C++ Assignments/EnrollmentsV2.cpp
@@ -7,8 +7,8 @@ using namespace std;
 int main()
 {
     string s;
-    int j = 0, k = 0, l = 0, m = 0, n = 0, o = 0, p = 0, q = 0, r = 0, t = 0;
     char size = 0;
+    int count[10] = {};
 
     ifstream myfile;
 
@@ -20,37 +20,16 @@ int main()
 
             switch (size) {
             case '1':
-                j++;
-                break;
             case '2':
-                k++;
-                break;
             case '3':
-                l++;
-                break;
             case '4':
-                m++;
-                break;
             case '5':
-                n++;
-                break;
             case '6':
-                o++;
-                break;
             case '7':
-                p++;
-                break;
             case '8':
-                q++;
-                break;
             case '9':
-                r++;
+                count[size - '1']++;
                 break;
-            default:
-                t++;
-                break;
-            
-
             }
         }
     }
@@ -60,18 +39,10 @@ int main()
 
     myfile.close();
 
+    for (int i = 0; i < 9; i++) {
+        cout << "Numbers that start with " << i + 1 << ": " << count[i] << endl;
+    }
 
-    cout << "Amount of numbers that start with 1: " << j << endl;
-    cout << "Amount of numbers that start with 2: " << k << endl;
-    cout << "Amount of numbers that start with 3: " << l << endl;
-    cout << "Amount of numbers that start with 4: " << m << endl;
-    cout << "Amount of numbers that start with 5: " << n << endl;
-    cout << "Amount of numbers that start with 6: " << o << endl;
-    cout << "Amount of numbers that start with 7: " << p << endl;
-    cout << "Amount of numbers that start with 8: " << q << endl;
-    cout << "Amount of numbers that start with 9: " << r << endl;
-
- 
     return 0;
 
 
