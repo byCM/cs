830a81560e300a7ec0661c4a17dc1836c325c2b7
Michael Ross <43051181+byCM@users.noreply.github.com>
2020-04-23 01:13:35 -0800

    Update c.cpp

--- C++ Assignments/Lab/Lab 13/c.cpp
+++ C++ Assignments/Lab/Lab 13/c.cpp
@@ -2,31 +2,31 @@
 #include <math.h>
 
 //method to get the user input
-void getValues(float *pv, float *r, int *n)
+void getValues(float* pv, float* r, int* n)
 {
-printf("Enter current amount (present value). \n");
-scanf("%f", pv);
-printf("Enter yearly interest rate. \n");
-scanf("%f", r);
-printf("Enter number of years of compound interest. \n");
-scanf("%d", n);
-  
+    printf("Enter current amount (present value). \n");
+    scanf_s("%f", pv);
+    printf("Enter yearly interest rate. \n");
+    scanf_s("%f", r);
+    printf("Enter number of years of compound interest. \n");
+    scanf_s("%d", n);
+
 }
 int main()
 {
-//variable declaration
-float pv, fv, r;
-int n;
-  
-//method calling
-//pass the address of the varible
-getValues(&pv, &r, &n);
-  
-//calculate the final value
-fv = pv * pow(1.0 +r, n);
-  
-//display the result
-printf("$%.2f after %d years with a compound interest rate of %.2f is $%.2f dollars.\n", pv, n, r, fv);
+    //variable declaration
+    float pv, fv, r;
+    int n;
 
-return 0;
+    //method calling
+    //pass the address of the varible
+    getValues(&pv, &r, &n);
+
+    //calculate the final value
+    fv = pv * pow(1.0 + r, n);
+
+    //display the result
+    printf("$%.2f after %d years with a compound interest rate of %.2f is $%.2f dollars.\n", pv, n, r, fv);
+
+    return 0;
 }
