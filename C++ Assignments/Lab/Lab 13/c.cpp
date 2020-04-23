f790fa8f0e225bc46c0c3017ee1e65481946ae03
Michael Ross <43051181+byCM@users.noreply.github.com>
2020-04-23 00:29:35 -0800

    Create c.cpp

---
+++ C++ Assignments/Lab/Lab 13/c.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+//method to get the user input
+void getValues(float *pv, float *r, int *n)
+{
+printf("Enter current amount (present value). \n");
+scanf("%f", pv);
+printf("Enter yearly interest rate. \n");
+scanf("%f", r);
+printf("Enter number of years of compound interest. \n");
+scanf("%d", n);
+  
+}
+int main()
+{
+//variable declaration
+float pv, fv, r;
+int n;
+  
+//method calling
+//pass the address of the varible
+getValues(&pv, &r, &n);
+  
+//calculate the final value
+fv = pv * pow(1.0 +r, n);
+  
+//display the result
+printf("$%.2f after %d years with a compound interest rate of %.2f is $%.2f dollars.\n", pv, n, r, fv);
+
+return 0;
+}
