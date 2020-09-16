bd6e03c9967448a5fb7b30123ba50b0dd5be6f0b
Michael <43051181+penguinphp@users.noreply.github.com>
2020-09-16 14:55:36 -0800

    Update A1MIPS.cpp

--- DSA/A248/A1MIPS.cpp
+++ DSA/A248/A1MIPS.cpp
@@ -1,8 +1,9 @@
+  
 .data				# #include <iostream>
 prompt: .asciiz			# #include <math.h>
-prompt1: .asciiz
+prompt1: .asciiz		#
 prompt2: .asciiz		# using namespace std;
-
+				#
 .globl main			# void countSet(unsigned long long decimal)
 .text				# {
 main:				#     unsigned long long binarynum = 0, temp = 1, num;
@@ -10,19 +11,19 @@ li $v0,4			#     int rem;
 la $a0,prompt 			#     num = decimal;
 syscall				#     int i = 0, set = 0;
 li $v0,5			#     int arr[32];
-syscall
+syscall				#
 move $t1,$v0			#     while (i < 32)
 move $t5,$t1			#     {
 				#         rem = decimal % 2;
 li $s0,0 			#         decimal /= 2;
-
+				#
 loop:				#         temp *= 10;
 div $t2,$t1,2 			#         arr[i] = rem;
 mfhi $t2			#         i++;
 beqz $t2, skip			#         if (rem == 1)
 add $s0,$s0,1 			#             set++;
 skip:				#     }
-srl $t1,$t1,1
+srl $t1,$t1,1			#
 bgtz $t1,loop			#     cout << endl << "Binary Representation: ";
 				#     {
 li $v0,4			#         for (int i = 31; i >= 0; i--)
@@ -30,16 +31,16 @@ la $a0,prompt1			#             cout << arr[i];
 syscall				#     }
 li $v0,1			#     cout << endl << endl << "There are exactly " << set << " bits set in the value " << num;
 move $a0,$s0			# }
-syscall
-li $v0,4
+syscall				#
+li $v0,4			#
 la $a0,prompt2			# int main()
 syscall				# {
 li $v0,1			#     long long num;
-move $a0,$t5
+move $a0,$t5			#
 syscall				#     cout << "Enter a decimal integer value: ";
 				#     cin >> num;
-
+				#
 				#     countSet(num);
-
+				#
 				#     return 0;
 				# }
