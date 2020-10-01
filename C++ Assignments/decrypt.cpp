91802350d1c43b67e8badb6e98cabc4c138433d8
Michael <43051181+penguinphp@users.noreply.github.com>
2020-10-01 14:47:37 -0800

    Update decrypt.cpp

--- C++ Assignments/decrypt.cpp
+++ C++ Assignments/decrypt.cpp
@@ -1,49 +1,28 @@
-
 #include <iostream>
-
 using namespace std;
 
 char code[] = ",vtaNm a_\"dabp!!";
 
-
-void decrypt(int key)
-
-{
+void decrypt(int key){
     if (key < 1 || key > 500) {
-
         cout << "Invalid key! Key should be between 1 to 500\n";
-
         return;
-
     }
 
     int i = 0;
-
     while (code[i] != '\0') {
-
         int* p = (int*)(code + i);
-
         *p -= key;
-
         i += 4;
-
     }
-
     cout << code << endl;
-
 }
 
 
 int main()
-
 {
-
     decrypt(491);
-
 }
-
-
-
 
 
 
@@ -70,24 +49,13 @@ void decrypt(int key)
         *convert = *convert - key;
         i += 4;
     } while (message[i] != '\0');
-
-
     cout << message << endl;
-
 }
 
 
-int main()
-
-{
-    
+int main(){    
     for (int i = 0; i < 500; i++) {
         cout << "Test Num: " << i << endl;
     }
-
-    
-    
-
-
 
 }
