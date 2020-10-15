73011a193f735a52455a5cd37aa0a900c93ed37d
Michael <43051181+penguinphp@users.noreply.github.com>
2020-10-14 16:39:20 -0800

    Update A2.cpp

--- DSA/A248/A2.cpp
+++ DSA/A248/A2.cpp
@@ -1,6 +1,7 @@
-.data
 
-#Inst: .#
+
+
+.data
 
 CountR: .word 0
 CountJ: .word 0
@@ -9,7 +10,6 @@ TypeR: .word 0
 TypeJ1: .word 2
 TypeJ2: .word 3
 EndI: .word 0
-
 
 .text
 main:
@@ -25,43 +25,40 @@ main:
 	li $s0, 0 
 	lw $t7, EndI
 
-Loop: #read instruction at address into $s0
-	lw $s0,0($t3)
-	#shift it right logical by 26 ,makes it 0000xxxx, $s0= bits 31-26 only
+Loop: 
+	lw $s0,0($t3) #read instruction $t3 into $s0
 	srl $s0,$s0,26
-	# check shifted instruction
-	beq $s0, $t4, typeR #if it's typeR, then jump to typeR
-	beq $s0, $t5, typeJ #if it's typeJ1, then jump to typeJ
-	beq $s0, $t6, typeJ #if it's typeJ2, then jump to typeJ
+	
+	beq $s0, $t4, typeR
+	beq $s0, $t5, typeJ
+	beq $s0, $t6, typeJ
 
-TypeI: #none of above, then it's typeI
-	addi $t2, $t2, 1 #countI +1
-	j Next #jump to next
+TypeI: 
+	addi $t2, $t2, 1 #countI++
+	j Next #JUMP
 
 typeR: # bit (31-26) is 0x00 (type r)
-	addi $t0, $t0, 1 #countR +1
-	j Next #jump to next
+	addi $t0, $t0, 1 #countR++
+	j Next #JUMP
 
 typeJ: # bit (31-26) is 0x02 or 0x03
-	addi $t1, $t1, 1 #countJ +1
-	j Next #jump to next
+	addi $t1, $t1, 1 #countJ++
+	j Next #JUMP
 
-Next: #after count, check if it's end instruction
-	beq $s0,$t7,Exit #if it's endI, jump to exit
-	#not end yet, add address to get another instruction
+Next: 
+	beq $s0,$t7,Exit # jump to exit if EndI
 	addi $t3,$t3,4 #address+4
 	j Loop
 
-Exit: #print result
-#----------
+Exit:
 	li $v0,4
-	la $a0, CountR
+	la $a0, CountR #PRINT
 	syscall
 	li $v0,4
-	la $a0, CountJ
+	la $a0, CountJ #PRINT
 	syscall
 	li $v0,4
-	la $a0, CountI
+	la $a0, CountI #PRINT
 	syscall
 	li $v0,10
 	syscall
