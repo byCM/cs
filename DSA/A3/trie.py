7342fcefd53c12bee293a2aa21dadcefbde96c46
Michael <43051181+penguinphp@users.noreply.github.com>
2020-10-25 15:36:37 -0800

    Update trie.py

--- DSA/A3/trie.py
+++ DSA/A3/trie.py
@@ -10,129 +10,110 @@ class TrieNode():
   
 class Trie():
   
-def __init__(self):     
-  
-self.root = TrieNode()
-self.word_list = []
-  
+def __init__(self):      
+   self.root = TrieNode()
+   self.word_list = []
+
 def insertAllKeys(self, words):
-for key in words:
-self.insert(self.root,key,key)
+   for key in words:
+   self.insert(self.root,key,key)
   
   
-def insert(self,node,remaining_word,original_word):
+def insert(self,node,remaining_word,original_word):  
+   if remaining_word == "":
+      return
+      c = remaining_word[0]     
+      w = ""
   
-if remaining_word == "":
-return
-  
-c = remaining_word[0]
-  
-w = ""
-  
-if len(remaining_word) == 1:
-w = ""
-node.last = True
-else :
-w = remaining_word[1:]
+   if len(remaining_word) == 1:
+      w = ""
+      node.last = True
+   else :
+      w = remaining_word[1:]
   
   
 if c not in node.children:
-node.children[c] = TrieNode()
-self.insert(node.children[c],w,original_word)
-else :
-self.insert(node.children[c],w,original_word)
+      node.children[c] = TrieNode()
+      self.insert(node.children[c],w,original_word)
+   else :
+      self.insert(node.children[c],w,original_word)
 
 
-def find(self,root, word):
-      
-if root == None:
-return False
-
-if word == "":
-return root.last
-  
-c = word[0]
-  
-w = ""
-if len(word) == 0:
-w = ""
-else :
-w = ""+word[1:]
+def find(self,root, word):    
+   if root == None:
+      return False
+   if word == "":
+      return root.last
+   c = word[0]
+   w = ""
+   if len(word) == 0:
+      w = ""
+   else :
+      w = ""+word[1:]
   
   
-if c not in root.children:
-return False
-else :
-return self.find(root.children[c],w)
+   if c not in root.children:
+      return False
+   else :
+      return self.find(root.children[c],w)
 
   
 def search_string(self, node, word):
+  if node.last:
+   self.word_list.append(word)
   
-  
-if node.last:
-self.word_list.append(word)
-  
-for a,n in node.children.items():
-self.search_string(n, word + a)
+   for a,n in node.children.items():
+   self.search_string(n, word + a)
   
 def search_string_based_on_prefix(self, node,key):
+  not_found = False
+   temp_word = ''
   
-not_found = False
-temp_word = ''
+   for a in list(key):
+      if not node.children.get(a):
+      not_found = True
+      break
   
-for a in list(key):
-if not node.children.get(a):
-not_found = True
-break
+   temp_word += a
+   node = node.children[a]
   
-temp_word += a
-node = node.children[a]
-  
-if not_found:
-return 0
-elif node.last and not node.children:
-return -1
+   if not_found:
+      return 0
+   elif node.last and not node.children:
+      return -1
   
 self.word_list = []
-  
 self.search_string(node, temp_word)
-  
 print("List of string with prefix ",key)
   
 for s in self.word_list:
-print(s)
-  
-return 1
+   print(s)
+   return 1
 
   
   
   
 def main():
-  
-file_name = input("Please enter the file name : ")
-keys = []
-  
+   file_name = input("Please enter the file name : ")
+   keys = [] 
 fopen = open(file_name,"r")
   
 for x in fopen:
-keys.append(x)
-  
-trie = Trie()
-  
-trie.insertAllKeys(keys)
+   keys.append(x)
+   trie = Trie()
+   trie.insertAllKeys(keys)
 
-word = input("Please enter the string to be search : ")
+   word = input("Please enter the string to be search : ")
   
-found = trie.find(trie.root,word)
+   found = trie.find(trie.root,word)
 
 if found == True:
-print("String is found")
+   print("String is found")
 else :
-print("String is not found")
+   print("String is not found")
 
 
 key = input("Please enter the prefix of strings to be search: ")   
-  
 value = trie.search_string_based_on_prefix(trie.root,key)
   
 if value == 0:
