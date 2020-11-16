8ea687343efa03cfec16a645e9c14727d292866d
Michael <43051181+penguinphp@users.noreply.github.com>
2020-11-15 16:49:46 -0900

    Create dbsysA10.cpp

---
+++ dbsysA10.cpp
@@ -0,0 +1,22 @@
+INSERT INTO INVOICE
+VALUES(10983,
+'10010',
+'11-May-2018',
+'55.55',
+ '30,
+       'OPEN');
+       
+INSERT INTO LINE
+VALUES (10983,
+       1,
+       '11QER/31',
+       1,
+       110.00);
+       
+UPDATE CUSTOMER
+SET CUS_DATELSTPUR = '11-May-2018',
+CUS_BALANCE = CUS_BALANCE + 55.55
+WHERE CUS_CODE = '10010';
+COMMIT;
+       
+       
